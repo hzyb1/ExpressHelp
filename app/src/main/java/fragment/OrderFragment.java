4472004e0c963664f4d510986fd3d7d050afa172
shaodong <1453431007@qq.com>
2019-03-28 20:19:20 +0800

    add fragment

--- app/src/main/java/fragment/OrderFragment.java
+++ app/src/main/java/fragment/OrderFragment.java
@@ -1,10 +1,20 @@
 package fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Administrator on 2019/3/28 0028.
  */
 
 public class OrderFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }

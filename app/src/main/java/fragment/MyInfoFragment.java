410758e0cdd586ac76dd321ec9556634b89b967e
Lei <597215781@qq.com>
2019-03-28 19:35:55 +0800

    add MyOrderActivity

--- app/src/main/java/fragment/MyInfoFragment.java
+++ app/src/main/java/fragment/MyInfoFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
 import com.example.a14574.expresshelp.R;
@@ -18,12 +19,14 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class MyInfoFragment extends Fragment {
     private Button setting;
     private CircleImageView headImage;
+    private LinearLayout pendingPaymentLayout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my_info,container,false);
         setting = (Button) view.findViewById(R.id.setting);
+        pendingPaymentLayout = (LinearLayout) view.findViewById(R.id.pending_payment_layout);//待支付
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +36,13 @@ public class MyInfoFragment extends Fragment {
         headImage = (CircleImageView) view.findViewById(R.id.head_image);
         Glide.with(getContext()).load(R.drawable.temp_head2).into(headImage);
         return view;
+    }
+    private void initEvents(){
+        pendingPaymentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }

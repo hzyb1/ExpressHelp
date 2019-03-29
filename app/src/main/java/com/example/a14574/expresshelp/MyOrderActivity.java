5c692d77f24ed0d31fcb524cd55cd474e589f4be
Lei <597215781@qq.com>
2019-03-29 16:30:11 +0800

    添加查看订单中的五个RatioButton，用来区分查询条件

--- app/src/main/java/com/example/a14574/expresshelp/MyOrderActivity.java
+++ app/src/main/java/com/example/a14574/expresshelp/MyOrderActivity.java
@@ -3,10 +3,13 @@ package com.example.a14574.expresshelp;
 
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,13 +20,30 @@ import model.Order;
 
 public class MyOrderActivity extends AppCompatActivity {
     private List<Order> orderList = new ArrayList<>();
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_order);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
         FragmentManager fragmentManager = getSupportFragmentManager();
-       FragmentTransaction transaction = fragmentManager.beginTransaction();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.order_fragment,new OrderFragment());
         transaction.commit();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

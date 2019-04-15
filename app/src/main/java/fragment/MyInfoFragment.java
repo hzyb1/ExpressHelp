736aa24a2beedf32e5c95d225a85eb5c1f494d63
hzyb1 <1457493024@qq.com>
2019-04-15 17:22:35 +0800

    我的订单修改完成

--- app/src/main/java/fragment/MyInfoFragment.java
+++ app/src/main/java/fragment/MyInfoFragment.java
@@ -18,7 +18,6 @@ import com.example.a14574.expresshelp.MyOrderActivity;
 import com.example.a14574.expresshelp.R;
 import com.example.a14574.expresshelp.SettingActivity;
 import com.example.a14574.expresshelp.SpecificUserInfoActivity;
-import com.example.a14574.expresshelp.TextActivity;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -112,7 +111,7 @@ public class MyInfoFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onClick(View view) {
         //Intent intent = new Intent(getActivity(), MyOrderActivity.class);
-        Intent intent = new Intent(getActivity(), TextActivity.class);
+        Intent intent = new Intent(getActivity(), MyOrderActivity.class);
         switch (view.getId()){
             case R.id.my_orders:
                 ;

2391b1560124e9e95953f26b427c98af3a4aa2f4
Lei <597215781@qq.com>
2019-04-20 18:16:17 +0800

    增加启动图片

--- app/src/main/java/com/example/a14574/expresshelp/BeginActivity.java
+++ app/src/main/java/com/example/a14574/expresshelp/BeginActivity.java
@@ -2,10 +2,14 @@ package com.example.a14574.expresshelp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -20,11 +24,19 @@ import okhttp3.Call;
 import okhttp3.Response;
 
 public class BeginActivity extends BaseActivity {
+    private ImageView beginImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         closeAndroidPDialog();
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_begin);
+        beginImage = (ImageView) findViewById(R.id.begin_image);
+        Glide.with(this).load(R.drawable.start_image).into(beginImage);
         spLogin();          //持久化登录
 
     }

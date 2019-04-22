7c74004ce5b8c327ba2db8efd7dd7c075d03b887
Lei <597215781@qq.com>
2019-04-22 18:22:14 +0800

    增加跑手查看订单详情界面

--- app/src/main/java/com/example/a14574/expresshelp/SettingActivity.java
+++ app/src/main/java/com/example/a14574/expresshelp/SettingActivity.java
@@ -19,9 +19,12 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -53,6 +56,7 @@ import util.ActivityCollector;
 
 public class SettingActivity extends BaseActivity {
 
+    private Toolbar toolbar;
     private Button logout;
     private CircleImageView headImage;
     private TextView userName;
@@ -96,7 +100,13 @@ public class SettingActivity extends BaseActivity {
             String url = this.getString(R.string.TheServer)+"headImages/"+ LoginActivity.USER.getHeadImage();
             Glide.with(this).load(url).into(headImage);;
         }
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
 
 
@@ -253,5 +263,15 @@ public class SettingActivity extends BaseActivity {
             cursor.close();
         }
         return path;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

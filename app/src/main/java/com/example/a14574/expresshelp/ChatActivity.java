417a723fa4f9a98da86d5dcae91d62c560a28c04
hzyb1 <1457493024@qq.com>
2019-05-04 17:52:14 +0800

    a

--- app/src/main/java/com/example/a14574/expresshelp/ChatActivity.java
+++ app/src/main/java/com/example/a14574/expresshelp/ChatActivity.java
@@ -1,6 +1,10 @@
 package com.example.a14574.expresshelp;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,14 +15,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import Adapter.ChatAdapter;
 import Adapter.ChatListAdapter;
+import Adapter.OrderBriefAdapter;
+import Adapter.RunnerOrderAdapter;
+import http.HttpUtil;
 import model.ChatRecord;
+import model.MyBean;
+import model.Order;
+import okhttp3.Call;
+import okhttp3.Response;
 
 public class ChatActivity extends AppCompatActivity {
     private EditText content;
@@ -26,13 +48,39 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private List<ChatRecord> chatRecords = new ArrayList<>();
     private Bitmap bitmap;
+    private ProgressDialog progressDialog;     //等待状态对话框
+    private int id1,id2;
+    private int conversationId;
+    Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String result = "";
+            result = msg.obj.toString();
+            if(msg.arg1 == 1){      //初始化record；
+                if(result.length() <= 5){
+                    Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss ").create();
+                    chatRecords = gson.fromJson(result, new TypeToken<List<ChatRecord>>(){}.getType());
+                    conversationId = chatRecords.get(0).getConversationId();
+                    ChatAdapter adapter = new ChatAdapter(chatRecords,bitmap);
+                    recyclerView.setAdapter(adapter);
+                }else{
+                    conversationId = Integer.parseInt(result);
+                }
+
+            }
+            progressDialog.dismiss();       //结束等待
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         bitmap = getIntent().getParcelableExtra("photo");
         initView();
-        init();
+        initList();
         content.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -48,8 +96,8 @@ public class ChatActivity extends AppCompatActivity {
             public void afterTextChanged(Editable editable) {
             }
         });
-        final int id1 = getIntent().getIntExtra("id1",0);
-        final int id2 = getIntent().getIntExtra("id1",0);
+        id1 = getIntent().getIntExtra("id1",0);
+        id2 = getIntent().getIntExtra("id2",0);
         recyclerView = (RecyclerView)findViewById(R.id.rv_chat_list);
         LinearLayoutManager layoutManager = new LinearLayoutManager(ChatActivity.this);
         recyclerView.setLayoutManager(layoutManager);
@@ -58,7 +106,7 @@ public class ChatActivity extends AppCompatActivity {
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ChatRecord record = new ChatRecord();
+                final ChatRecord record = new ChatRecord();
                 record.setSenderId(LoginActivity.USER.getId());
                 if (LoginActivity.USER.getId() == id1){
                     record.setGeterId(id2);
@@ -67,8 +115,24 @@ public class ChatActivity extends AppCompatActivity {
                 }
                 record.setMessage(content.getText().toString());
                 record.setSendTime(new Timestamp(System.currentTimeMillis()));
-
-
+                if(conversationId == 0){
+                    initList();
+                }else{
+                    record.setConversationId(conversationId);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                OutputStream outputStream = LoginActivity.socket.getOutputStream();
+                                String text = new Gson().toJson(record);
+                                outputStream.write(text.getBytes("utf-8"));
+                                outputStream.flush();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+                }
             }
         });
 
@@ -77,17 +141,47 @@ public class ChatActivity extends AppCompatActivity {
         content = (EditText)findViewById(R.id.et_content);
         send = (ImageView)findViewById(R.id.ivAdd);
     }
-    public void init(){
-        for (int i = 0;i<2;i++) {
-            ChatRecord record = new ChatRecord();
-            record.setSenderId(10010);
-            record.setMessage("测试聊天内容");
-            chatRecords.add(record);
-
-            ChatRecord record1 = new ChatRecord();
-            record.setSenderId(10086);
-            record.setMessage("测试聊天内容");
-            chatRecords.add(record1);
+    public void initList(){
+        progressDialog = new ProgressDialog(ChatActivity.this);
+        progressDialog.setTitle("请稍后......");
+        progressDialog.setMessage("正在加载......");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        HashMap<String, String> params = new HashMap<String, String>();
+        if(LoginActivity.USER == null){
+            progressDialog.dismiss();
+            return ;
+        }else{
+        }
+        params.put("id1", id1+"");
+        params.put("id2", id2+"");
+        try {
+            //构造完整URL
+            String originAddress = this.getString(R.string.TheServer) +  "chatRecordsServlet";
+            String compeletedURL = HttpUtil.getURLWithParams(originAddress, params);
+            Log.d("日志",compeletedURL);
+            HttpUtil.sendGetOkHttpRequest(compeletedURL,new okhttp3.Callback(){
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Looper.prepare();
+                    Toast.makeText(ChatActivity.this,"未能连接到网络", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                    Looper.loop();
+                }
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if(!response.isSuccessful()){
+                        progressDialog.dismiss();
+                        return ;
+                    }
+                    Message message = new Message();
+                    message.obj = response.body().string().trim();
+                    message.arg1 = 1;
+                    mHandler.sendMessage(message);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/com/example/a14574/practice/ChatService.java
+++ app/src/main/java/com/example/a14574/practice/ChatService.java
@@ -3,25 +3,53 @@ package com.example.a14574.practice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.a14574.expresshelp.LoginActivity;
+import com.example.a14574.expresshelp.R;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import Adapter.RunnerOrderAdapter;
 import model.ChatRecord;
+import model.Order;
 
 public class ChatService extends Service {
-    private ChatBinder mBinder = new ChatBinder();
-    public class ChatBinder extends Binder{
-        private void addMessage(){//发来信息
-
+    Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String result = "";
+            result = msg.obj.toString();
+            if(msg.what == 1){
+                Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss ").create();
+                ChatRecord chatRecord = gson.fromJson(result, ChatRecord.class);
+                if(chatRecord.getGeterId() == LoginActivity.USER.getId()){      //处理发送给我的信息
+                    Intent intent1 = new Intent("CHAT_LIST");
+                    intent1.putExtra("record", chatRecord);
+                    sendBroadcast(intent1);
+                }
+            }
         }
-
-        private void sendMessage(){//发送信息
-
-        }
-    }
+    };
 
 
     public ChatService() {
+
+
+
+
     }
 
     @Override
@@ -29,20 +57,48 @@ public class ChatService extends Service {
         Log.d("日志","服务被启动了");
 
         super.onCreate();
+
+
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Intent intent1 = new Intent("CHAT_LIST");
-        ChatRecord record = new ChatRecord();
-        intent1.putExtra("record", record);
-        sendBroadcast(intent1);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //                  socket = new Socket(ip, 10010);
+                    //        new ClientSender(socket).send();
+                    if(LoginActivity.socket != null){
+                        return ;
+                    }
+                    InputStream inputStream = LoginActivity.socket.getInputStream();
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = inputStream.read(buffer)) != -1) {
+                        String data = new String(buffer, 0, len);
+                        // 发到主线程中 收到的数据
+                        Message message = Message.obtain();
+                        message.what = 1;
+                        message.obj = data;
+                        mHandler.sendMessage(message);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
+
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        return mBinder;
+        return null;
     }
 }

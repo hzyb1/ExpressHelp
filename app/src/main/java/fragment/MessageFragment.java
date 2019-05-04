1fc15761b93cf93e759c5d716b5f459618dac579
sd <1453431007@qq.com>
2019-05-05 02:26:53 +0800

    更新UI

--- app/src/main/java/fragment/MessageFragment.java
+++ app/src/main/java/fragment/MessageFragment.java
@@ -37,6 +37,7 @@ import model.ConversationVo;
 import model.Order;
 import okhttp3.Call;
 import okhttp3.Response;
+import util.SpaceItemDecoration;
 
 
 public class MessageFragment extends Fragment {
@@ -87,6 +88,7 @@ public class MessageFragment extends Fragment {
         adapter = new ChatListAdapter(conversationList,getContext());
         recyclerView.setAdapter(adapter);
         chatListReceiver = new ChatListReceiver();
+        recyclerView.addItemDecoration(new SpaceItemDecoration(10));
         return view;
     }
 
